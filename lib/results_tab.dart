aeb01acb3050aa3d611f30357089066868dfdf99
kirrlqh <kirilltrifonow18@gmail.com>
2024-12-17 18:19:18 +0400

    second commit

--- lib/results_tab.dart
+++ lib/results_tab.dart
@@ -10,27 +10,58 @@ class _ResultsTabState extends State<ResultsTab> {
   final TextEditingController _searchController = TextEditingController();
   List<Map<String, dynamic>> results = []; // Данные таблицы результатов
   List<Map<String, dynamic>> filteredResults = []; // Отфильтрованные результаты
+  bool isLoading = true; // Состояние загрузки данных
+
+  // Функция для получения фамилий студентов
+  Future<String> fetchStudentName(int studentId) async {
+    final response = await Supabase.instance.client
+        .from('students')
+        .select('surname')
+        .eq('id', studentId)
+        .single();
+
+    if (response != null && response['surname'] != null) {
+      return response['surname'];
+    } else {
+      return 'Неизвестно'; // Если фамилия не найдена
+    }
+  }
 
   // Функция для получения данных из таблицы results
-  Future<List<Map<String, dynamic>>> fetchResults() async {
-    final response = await Supabase.instance.client
-        .from('results')
-        .select('id, studentid, score, numberschool, dateevent, subject, students(surname)');
+  Future<void> fetchResults() async {
+    try {
+      final response = await Supabase.instance.client
+          .from('results')
+          .select('id, studentid, score, numberschool, dateevent, subject');
 
-    // Проверяем, является ли ответ списком
-    if (response is List) {
-      return List<Map<String, dynamic>>.from(response.map((result) {
-        return {
-          'id': result['id'],
-          'student': result['students']['surname'], // Фамилия студента
-          'score': result['score'],
-          'numberschool': result['numberschool'],
-          'dateevent': result['dateevent'],
-          'subject': result['subject'],
-        };
-      }));
-    } else {
-      throw Exception('Ошибка загрузки результатов.');
+      if (response is List) {
+        List<Map<String, dynamic>> fetchedResults = [];
+
+        for (var result in response) {
+          final studentName = await fetchStudentName(result['studentid']);
+          fetchedResults.add({
+            'id': result['id'],
+            'student': studentName, // Добавляем фамилию студента
+            'score': result['score'],
+            'numberschool': result['numberschool'],
+            'dateevent': result['dateevent'],
+            'subject': result['subject'],
+          });
+        }
+
+        setState(() {
+          results = fetchedResults;
+          filteredResults = fetchedResults; // Изначально показываем все результаты
+          isLoading = false; // Загрузка завершена
+        });
+      } else {
+        throw Exception('Ошибка загрузки результатов.');
+      }
+    } catch (e) {
+      print('Ошибка загрузки данных: $e');
+      setState(() {
+        isLoading = false; // Завершаем загрузку даже в случае ошибки
+      });
     }
   }
 
@@ -47,68 +78,59 @@ class _ResultsTabState extends State<ResultsTab> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<List<Map<String, dynamic>>>(
-      future: fetchResults(), // Получаем результаты с помощью fetchResults
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator()); // Показать индикатор загрузки
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Ошибка: ${snapshot.error}')); // Показать ошибку
-        } else if (snapshot.hasData) {
-          final results = snapshot.data!;
-          // Изначально показываем все результаты
-          if (filteredResults.isEmpty) {
-            filteredResults = results;
-          }
+  void initState() {
+    super.initState();
+    fetchResults(); // Загружаем результаты при инициализации
+  }
 
-          return Column(
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _searchController,
-                        decoration: InputDecoration(
-                          labelText: 'Поиск по фамилии студента',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: () {
-                        _filterResults(_searchController.text); // Запуск фильтрации по кнопке
-                      },
-                    ),
-                  ],
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    labelText: 'Поиск по фамилии студента',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: filteredResults.length,
-                  itemBuilder: (context, index) {
-                    final result = filteredResults[index];
-                    return Card(
-                      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                      child: ListTile(
-                        title: Text(result['student'] ?? 'Неизвестно'),
-                        subtitle: Text(
-                          'Школа: ${result['numberschool']}, Предмет: ${result['subject']}, Балл: ${result['score']}, Дата: ${result['dateevent']}',
-                        ),
-                      ),
-                    );
-                  },
-                ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  _filterResults(_searchController.text); // Запуск фильтрации по кнопке
+                },
               ),
             ],
-          );
-        } else {
-          return Center(child: Text('Нет данных.'));
-        }
-      },
+          ),
+        ),
+        Expanded(
+          child: isLoading
+              ? Center(child: CircularProgressIndicator()) // Показать индикатор загрузки
+              : (filteredResults.isEmpty
+              ? Center(child: Text('Нет данных.'))
+              : ListView.builder(
+            itemCount: filteredResults.length,
+            itemBuilder: (context, index) {
+              final result = filteredResults[index];
+              return Card(
+                margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                child: ListTile(
+                  title: Text(result['student'] ?? 'Неизвестно'),
+                  subtitle: Text(
+                    'Школа: ${result['numberschool']}, Предмет: ${result['subject']}, Балл: ${result['score']}, Дата: ${result['dateevent']}',
+                  ),
+                ),
+              );
+            },
+          )),
+        ),
+      ],
     );
   }
 }
